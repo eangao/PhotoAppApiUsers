3bc805c36675db1604d1e503e7ff96dcb0283786
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-18 14:33:38 +0800

    Photo App Api Users initial setup

---
+++ src/main/java/com/appsdeveloper/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,15 @@
+package com.appsdeveloper.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class PhotoAppApiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
+
+}
