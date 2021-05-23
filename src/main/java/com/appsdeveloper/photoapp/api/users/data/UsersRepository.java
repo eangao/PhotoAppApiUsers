11383eea46fc396bb69f7ea08a077aee2a30ba56
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-23 14:32:52 +0800

    Implementing Spring Data JPA CRUD Repository -  closes # 13

---
+++ src/main/java/com/appsdeveloper/photoapp/api/users/data/UsersRepository.java
@@ -0,0 +1,6 @@
+package com.appsdeveloper.photoapp.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+}
