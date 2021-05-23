75af9a55fd61d8c50d4ae9cbb9c3c0c805b6c0bb
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-23 13:54:43 +0800

    Implementing Service Layer Class -  closes #

---
+++ src/main/java/com/appsdeveloper/photoapp/api/users/service/UserService.java
@@ -0,0 +1,5 @@
+package com.appsdeveloper.photoapp.api.users.service;
+
+public interface UserService {
+    UserDto createUser(UserDto userDetails);
+}
---
+++ src/main/java/com/appsdeveloper/photoapp/api/users/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.appsdeveloper.photoapp.api.users.service;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        return null;
+    }
+}
